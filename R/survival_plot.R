00a9252f896491df68beb94a67d7dbdb18284968
NickTedesco <nrt21@pitt.edu>
2022-04-14 13:42:21 -0400

    percent fix for survival plot (turned back into proportions to prevent autoscaling)

--- R/survival_plot.R
+++ R/survival_plot.R
@@ -22,7 +22,6 @@ survival_plot <- function(lifeTable) {
            + geom_step() + theme_linedraw()
            + labs(x = "Age", y = "Survival", title = "Life Table Estimate of Survival by Age")
            + scale_x_continuous(breaks = lifeTable$counter, labels = lifeTable$age)
-           + scale_y_continuous(labels = scales::percent)
            + theme(plot.title = element_text(hjust = 0.5)))
   return(plot)
 }
