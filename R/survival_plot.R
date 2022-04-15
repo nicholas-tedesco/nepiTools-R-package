00080ed11ede5c5e47bd537f132cef30a62ab48b
NickTedesco <nrt21@pitt.edu>
2022-04-15 03:46:51 -0400

    life table fix 2

--- R/survival_plot.R
+++ R/survival_plot.R
@@ -23,14 +23,14 @@ survival_plot <- function(lifeTable) {
            + geom_step() + theme_linedraw()
            + labs(x = "Age", y = "Survival", title = "Life Table Estimate of Survival by Age")
            + scale_x_continuous(breaks = lifeTable$counter, labels = lifeTable$age)
-           + scale_y_continuous(labels = scales::percent, limits = c(0, 1), )
+           + scale_y_continuous(labels = scales::percent, limits = c(0, 1))
            + theme(plot.title = element_text(hjust = 0.5)))
   } else {
     plot <- (ggplot(data = lifeTable, aes(x = counter, y = survival_prop))
              + geom_step() + theme_linedraw()
              + labs(x = "Age", y = "Survival", title = "Life Table Estimate of Survival by Age")
              + scale_x_continuous(n.breaks = 10)
-             + scale_y_continuous(labels = scales::percent, limits = c(0, 1), )
+             + scale_y_continuous(labels = scales::percent, limits = c(0, 1))
              + theme(plot.title = element_text(hjust = 0.5)))
   }
   return(plot)
